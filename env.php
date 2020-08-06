488666c1fa45bfd211fb7939154132c38ba0f333
Cameron Campbell <cameronccampbell1997@gmail.com>
2020-08-06 10:22:18 +0100

    Sort through env

--- env.php
+++ env.php
@@ -8,5 +8,9 @@ return array(
     'VIEW_CACHE_DISABLED' => TRUE,
     'PUBLISH_KEY' => 'pk_test_51Gu0wUDIohyHdIVWTWOhBpA9W8w8C9UaTwgRrHmxRbj43bbjpfvDVSPl96TBS6ijSgP5WdPCCOxdrPMBGXkzNYu800CL3t39Og',
     'SECRET_KEY' => 'sk_test_51Gu0wUDIohyHdIVW4M2AxBM3xQ1uLUGf9LIHWDY3CozrEBsbj65d5eKg8QDTfzcEqlD3orM8bqd5mGvV7GcYX0hg00RNZT8rxT',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'work',
+    'DB_USERNAME' => 'root',
+    'DB_PASSWORD' => 'root',
     
 );
